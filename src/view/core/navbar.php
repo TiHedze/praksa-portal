fb70aecd7e516471d4299e954ece2696981b4b57
Tilen Hedžet <tilen.hedzet@student.math.hr>
2021-07-11 22:41:00 +0200

    changed navbar theme

--- src/view/core/navbar.php
+++ src/view/core/navbar.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-dark bg-primary">
   <div class="container-fluid">
     <a href="#" class="navbar-brand">Profil</a>
     <form class="d-flex" method="POST" action="index.php?rt=ad/search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="companyName">
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <button class="btn bg-white" type="submit">Search</button>
     </form>
   </div>
 </nav>
